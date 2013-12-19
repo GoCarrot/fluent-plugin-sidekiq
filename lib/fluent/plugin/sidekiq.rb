9183aedf35a946df26e664abffe4dbfb47396aa9
Alex Scarborough <alex@gameclay.com>
2013-12-19 15:21:04 -0800

    Initial commit

---
+++ lib/fluent/plugin/sidekiq.rb
@@ -0,0 +1,9 @@
+require "fluent/plugin/sidekiq/version"
+
+module Fluent
+  module Plugin
+    module Sidekiq
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent/plugin/sidekiq/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Sidekiq
+      VERSION = "0.0.1"
+    end
+  end
+end
