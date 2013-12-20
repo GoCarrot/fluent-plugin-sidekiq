8e8381d998b996c0f3d78d0f541c7758f3c4b5c2
Alex Scarborough <alex@gameclay.com>
2013-12-19 16:10:49 -0800

    First pass at a sidekiq output plugin

---
+++ lib/fluent/plugin/out_sidekiq.rb
@@ -0,0 +1,48 @@
+require "fluent/plugin/sidekiq/version"
+require "redis"
+
+class Fluent::SidekiqOutput < Fluent::BufferedOutput
+  Fluent::Plugin.register_output("sidekiq", self)
+
+  config_param :redis_url, :string, :default => 'redis://localhost:6379'
+  config_param :redis_namespace, :string, :default => nil
+
+  def start
+    super
+  end
+
+  def shutdown
+    super
+  end
+
+  def format(tag, time, record)
+    [tag, time, record].to_msgpack
+  end
+
+  def redis_client
+    opts = {url: @redis_url}
+    client = Redis.new(opts)
+    if @redis_namespace
+      require "redis/namespace"
+      Redis::Namespace.new(@redis_namespace, client)
+    else
+      client
+    end
+  end
+
+  def write(chunk)
+    client = redis_client
+    client.pipelined do
+      chunk.msgpack_each do |tag, time, data|
+        at = data.delete('at')
+        if at
+          client.zadd('schedule', [at, data['payload']])
+        else
+          queue = data.delete('queue')
+          client.sadd('queues', queue)
+          client.lpush("queue:#{queue}", data['payload'])
+        end
+      end
+    end
+  end
+end
--- lib/fluent/plugin/sidekiq.rb
+++
@@ -1,9 +0,0 @@
-require "fluent/plugin/sidekiq/version"
-
-module Fluent
-  module Plugin
-    module Sidekiq
-      # Your code goes here...
-    end
-  end
-end
