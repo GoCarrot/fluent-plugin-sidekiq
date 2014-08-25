5d86cdafcfb9b9cbf05257a445a14736586a78ae
Alex Scarborough <alex@gameclay.com>
2014-08-25 11:35:46 -0700

    Auto-batch jobs that go through us

--- lib/fluent/plugin/out_sidekiq.rb
+++ lib/fluent/plugin/out_sidekiq.rb
@@ -1,11 +1,38 @@
 require "redis"
 
 class Fluent::SidekiqOutput < Fluent::BufferedOutput
-  VERSION = "0.0.2"
+  class Batch
+    attr_accessor :queue, :klass, :payload
+
+    def initialize(q, k)
+      self.queue = q
+      self.klass = k
+      self.payload = {:class => k, :retry => true, :jid => "", :enqueued_at => 0, :args => [[]] }
+    end
+
+    def acceptable_batch(q, c, max_size)
+      q == queue && c == klass && payload[:args][0].length < max_size
+    end
+
+    def add_to_batch(p)
+      payload[:retry] = p['retry']
+      payload[:jid] = p['jid']
+      payload[:enqueued_at] = p['enqueued_at']
+      payload[:args][0] << p['args'][0][0]
+    end
+
+    def enqueue(client)
+      client.sadd("queues", queue)
+      client.lpush("queue:#{queue}", JSON.generate(payload))
+    end
+  end
+
+  VERSION = "0.0.3"
   Fluent::Plugin.register_output("sidekiq", self)
 
   config_param :redis_url, :string, :default => 'redis://localhost:6379'
   config_param :redis_namespace, :string, :default => nil
+  config_param :max_batch_size, :integer, :default => 5
 
   def start
     super
@@ -32,17 +59,33 @@ class Fluent::SidekiqOutput < Fluent::BufferedOutput
 
   def write(chunk)
     client = redis_client
-    client.multi do
-      chunk.msgpack_each do |tag, time, data|
-        at = data.delete('at')
-        if at
-          client.zadd('schedule', [at, data['payload']])
-        else
-          queue = data.delete('queue')
-          client.sadd('queues', queue)
-          client.lpush("queue:#{queue}", data['payload'])
+    scheduled_jobs = []
+    batches = []
+
+    chunk.msgpack_each do |tag, time, data|
+      at = data.delete('at')
+      if at
+        scheduled_jobs << [at, data['payload']]
+      else
+        payload = JSON.parse(data['payload'])
+        queue = data.delete('queue')
+        klass = payload['class']
+
+        batch = batches.find { |b| b.acceptable_batch(queue, klass, max_batch_size) }
+        if !batch
+          batch = Batch.new(queue, klass)
+          batches << batch
         end
+
+        batch.add_to_batch(payload)
       end
+    end
+
+    if scheduled_jobs.length > 0
+      client.zadd('schedule', scheduled_jobs)
+    end
+    batches.each do |batch|
+      batch.enqueue(client)
     end
   end
 end
